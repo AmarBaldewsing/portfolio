139d9bcf9155ee9de5f973cb5fa65a32df82a070
AmarBaldewsing <115461124+AmarBaldewsing@users.noreply.github.com>
2024-05-31 11:07:24 +0200

    Delete portfolio-amar directory

--- portfolio-amar/src/hooks/useDocTitle.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-const useDocumentTitle = (page) => {
-    useEffect(() => {
-        document.title = 'Amar Baldewsing - ' + page;
-    }, [page])
-}
- 
-export default useDocumentTitle;
